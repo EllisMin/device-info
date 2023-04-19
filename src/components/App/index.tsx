93477739308067e9ff5271cf4d2cccb83d2f7cc7
ellismin <dbmin@hotmail.com>
2023-04-19 01:36:04 -0500

    Display device and local times

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,6 +3,7 @@ import { Card } from '../Card'
 import './styles.scss'
 import { isMobile } from 'react-device-detect'
 import { Section } from '../Section'
+import moment from 'moment-timezone'
 
 type Geolocation = {
   country_name: string
@@ -24,6 +25,9 @@ function App() {
   const [navigator, setNavigator] = useState<Navigator | null>(null)
   const [geolocation, setGeolocation] = useState<Geolocation | null>(null)
   const [dns, setDns] = useState<Dns | null>()
+  const [deviceTime, setDeviceTime] = useState<Date>(new Date())
+  const [realTime, setRealTime] = useState<Date>(new Date())
+  const [timezone, setTimezone] = useState<null | string>(null)
 
   useEffect(() => {
     // Set navigator from window obj
@@ -48,7 +52,38 @@ function App() {
     }
 
     fetchIp()
+
+    const deviceTimeTimer = setInterval(() => {
+      setDeviceTime(new Date())
+    }, 1000)
+
+    const realTimer = setInterval(() => {
+      fetch('http://worldtimeapi.org/api/ip')
+        .then((res) => res.json())
+        .then((data) => {
+          setRealTime(new Date(data.datetime))
+          setTimezone(data.timezone)
+        })
+        .catch((err) => console.log(err))
+    }, 1000)
+
+    return () => {
+      clearInterval(deviceTimeTimer)
+      clearInterval(realTimer)
+    }
   }, [])
+
+  const formatDate = (date: Date) => {
+    const newDate = moment(date)
+    const pattern = `YYYY-MM-DD, hh:mm:ss A, ddd (UTCZ z)`
+    const isDst = newDate.tz(moment.tz.guess()).isDST()
+    const dstStr = `(DST: ${isDst ? 'Yes' : 'No'})`
+
+    if (timezone) {
+      return newDate.tz(timezone).format(pattern) + dstStr
+    }
+    return newDate.format(pattern) + dstStr
+  }
 
   return (
     <div className="App">
@@ -68,6 +103,11 @@ function App() {
           description={`${geolocation?.latitude}, ${geolocation?.longitude}`}
         />
         <Card title={'ISP provider'} description={`${dns?.geo}`} />
+      </Section>
+      <Section>
+        <Card title={'Device time'} description={formatDate(deviceTime)} />
+        <Card title={'Local time'} description={formatDate(realTime)} />
+        <Card title={'Local timezone'} description={timezone || ''} />
       </Section>
       <Card title={'User Agent'} description={navigator?.userAgent} />
 
